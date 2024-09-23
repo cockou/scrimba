4a838288cfd50dde54ed90535b27e68abb8a1fc4
cockou <roluma.7@gmail.com>
2024-09-22 22:44:37 -0600

    set up the erase button too

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-// document.getElementById("count").innerText = 5
+let countEl = document.getElementById("count-el")
 
-// change the count-el in the HTML to reflect the new count
+console.log(countEl)
 
 let count = 0
 
 function increment() {
     count = count + 1
-    console.log(count)
+    countEl.innerText = count
 }
 
-
+function erase() {
+    count = 0
+    countEl.innerText = count
+}
